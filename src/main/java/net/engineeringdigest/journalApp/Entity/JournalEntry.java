172262b95ade6b6b417f36032418716003de0b1f
Masum <abdullah.almasum@wsd.com>
2024-08-11 03:12:10 +0600

    Project Lombok annotation used

--- src/main/java/net/engineeringdigest/journalApp/Entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/Entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package net.engineeringdigest.journalApp.Entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,43 +9,11 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
-
     @Id
     private ObjectId id;
     private String title;
     private String content;
     private LocalDateTime creationTime;
-    
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getCreationTime() {
-        return creationTime;
-    }
-
-    public void setCreationTime(LocalDateTime creationTime) {
-        this.creationTime = creationTime;
-    }
 }
